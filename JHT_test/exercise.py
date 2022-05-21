bc66fc9ab9701d0dae6a5302017a6f3fb2c336c0
jht923 <104566836+jht923@users.noreply.github.com>
2022-05-21 20:32:10 +0900

    Update exercise.py

--- JHT_test/exercise.py
+++ JHT_test/exercise.py
@@ -3,10 +3,15 @@ import time                     # 타이머 사용
 from keypoint import KEYPOINT   # keypoint 불러오기
 from utils import *             # utils 불러오기
 
+import pygame
+
 # 전역 변수로 사용(타이머 구현)
 cur = 0.0
 prev = 0.0
 timeElapsed = 0.0  
+
+pushup_end = 1
+
 
 class EXERCISE(KEYPOINT):
     def __init__(self, landmarks):
@@ -95,7 +100,7 @@ class EXERCISE(KEYPOINT):
                     if avg_spine_angle > 160:
 
 
-                        if (avg_arm_angle < 90) and (elbow_angle < 110) :      # 무릎 충분히 굽혔을 때
+                        if (avg_arm_angle < 90) and (elbow_angle < 120) :      # 무릎 충분히 굽혔을 때
                             reps += 1               # 운동 동작 카운트                      
                             prev = time.time()      # 현재 시간 저장 -> reps == 5가 되는 순간 더 이상 갱신이 안되기 때문에 세트가 끝난 시간이라고 볼 수 있음          
                             status = 'Down'         # 운동 상태
@@ -103,50 +108,116 @@ class EXERCISE(KEYPOINT):
 
 
 
-                    if (elbow_angle > 110) and (avg_spine_angle < 160) :
-                        feedback = 'straighten your back andpytho Bring your elbows together a little more'
+                    if (elbow_angle > 120) and (avg_spine_angle < 160) :
+                        feedback = 'straighten your back and Bring your elbows together a little more'
+
+                        
+                        pygame.mixer.init() #소리를 재생할때마다 꼭 초기화 해야됨
+                        
+                        if pygame.mixer.music.get_busy() == False : #소리가 겹쳐서 재생되지 않도록 재생상태가 아닐때만 재생
+                            pygame.mixer.music.load('spineelbow.mp3')
+                            pygame.mixer.music.play()
+                       
+    
+
 
                     elif (avg_spine_angle < 160):
                         feedback = 'straighten your back'
 
-                    elif elbow_angle > 110:
+                        pygame.mixer.init() #소리를 재생할때마다 꼭 초기화 해야됨
+                        
+                        if pygame.mixer.music.get_busy() == False :
+                            pygame.mixer.music.load('spine.mp3')
+                            pygame.mixer.music.play()
+                       
+                       
+                   
+
+                    elif elbow_angle > 120:
                         feedback = 'Bring your elbows together a little more'
 
+                        pygame.mixer.init() #소리를 재생할때마다 꼭 초기화 해야됨
+                        
+                        if pygame.mixer.music.get_busy() == False :
+                            pygame.mixer.music.load('elbow.mp3')
+                            pygame.mixer.music.play()
+                            
 
 
 
 
                 else:
-
+                    
                     if avg_spine_angle > 160:
 
 
-                        if( avg_arm_angle > 160)and (elbow_angle < 110) :     # 무릎 충분히 폈을 때
+                        if( avg_arm_angle > 160)and (elbow_angle < 120) :     # 무릎 충분히 폈을 때
                             status = 'Up'           # 운동 상태 
                             feedback = 'Ready'      # 피드백 
 
 
                             
-                    if (elbow_angle > 110) and (avg_spine_angle < 160) :
+                    if (elbow_angle > 120) and (avg_spine_angle < 160) :
                         feedback = 'straighten your back and Bring your elbows together a little more'
+
+                        pygame.mixer.init() #소리를 재생할때마다 꼭 초기화 해야됨
+                        
+                        if pygame.mixer.music.get_busy() == False :
+                            pygame.mixer.music.load('spineelbow.mp3')
+                            pygame.mixer.music.play()
+
 
                     elif (avg_spine_angle < 160):
                         feedback = 'straighten your back'
+                       
+                        pygame.mixer.init() #소리를 재생할때마다 꼭 초기화 해야됨
+                        
+                        if pygame.mixer.music.get_busy() == False :
+                            pygame.mixer.music.load('spine.mp3')
+                            pygame.mixer.music.play()
+                    
+                           
 
-                    elif elbow_angle > 110:
+                    elif elbow_angle > 120:
                         feedback = 'Bring your elbows together a little more'
                 
-
+                        
+                        pygame.mixer.init() #소리를 재생할때마다 꼭 초기화 해야됨
+                        
+                        if pygame.mixer.music.get_busy() == False :
+                            pygame.mixer.music.load('elbow.mp3')
+                            pygame.mixer.music.play()
+                            
+                            
 
                         
             else:
                 if reps == 5:                   # reps가 끝나게 되면
                     # print('run timer')
                     reps, status, sets, feedback, timer = self.Rest_timer(reps, status, sets, feedback, timer)  # 타이머 함수 호출
+
+                    pygame.mixer.init() #소리를 재생할때마다 꼭 초기화 해야됨
+                        
+                    if pygame.mixer.music.get_busy() == False :
+                        pygame.mixer.music.load('buzer.mp3')
+                        pygame.mixer.music.play()
+           
+                            
         else:
             if sets == 3:                       # sets가 끝나게 되면
                 # print('운동 끝')                # 아직 별다른 조치 안함
                 feedback = 'well done!'         # 피드백
+
+                pygame.mixer.init() #소리를 재생할때마다 꼭 초기화 해야됨
+                global pushup_end #1번만 재생 하기위한 플래그
+                        
+                if (pygame.mixer.music.get_busy() == False) and pushup_end == 1 :
+                    pygame.mixer.music.load('pushupend.mp3')
+                    pygame.mixer.music.play()
+                    pushup_end = 0
+
+       
+
                 pass
         return [reps, status, sets, feedback,timer]
     
@@ -160,3 +231,4 @@ class EXERCISE(KEYPOINT):
                 reps, status, sets, feedback, timer)
         
         return [reps, status, sets, feedback, timer]
+    
