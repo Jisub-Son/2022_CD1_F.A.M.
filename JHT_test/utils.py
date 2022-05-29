5e7d31fe10efa23e7a8c5c74cb3a14603df43144
jht923 <104566836+jht923@users.noreply.github.com>
2022-05-29 15:54:53 +0900

    Update utils.py

--- JHT_test/utils.py
+++ JHT_test/utils.py
@@ -1,59 +1,107 @@
-import mediapipe as mp ## 스켈레톤 구현
-import pandas as pd ## keypoint간 빠른계산
-import numpy as np ## 스켈레톤 다차원 배열
-import cv2 ## opencv
+import mediapipe as mp 
+import pandas as pd 
+import numpy as np
+import cv2 
+import pygame
 
-mp_pose = mp.solutions.pose ## 스켈레톤
+# set constants for reference
+REF_TIMER = 5           
+REF_VISIBILITY = 0.7    
+REF_REPS = 5            
+REF_SETS = 3         
+RIGHT_CAM = 0
+LEFT_CAM = 1 
 
-def calculate_angle(a, b, c): ## 각도계산 로직(라디안 -> 각도)
-    a = np.array(a) ## 좌표값
+pygame.init()               # init mixer
+mp_pose = mp.solutions.pose # landmark
+
+# calculate length function
+def calculate_length(a, b):
+    # get np array(coordinates)
+    a = np.array(a)
+    b = np.array(b)
+    
+    length = np.hypot(a[0] - b[0], a[1] - b[1]) # 두 점 사이 거리
+    
+    return length
+
+# calculate angle function(ladian -> angle)
+def calculate_angle(a, b, c):
+    # get np array(coordinates)
+    a = np.array(a)
     b = np.array(b)
     c = np.array(c)
+    
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0]) # 라디안 계산
+    angle = np.abs(radians * 180.0 / np.pi) # 라디안 -> 각도(3.14로 나누기)
 
-    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0]) ## 라디안 계산
-    angle = np.abs(radians * 180.0 / np.pi) ## 라디안 -> 각도(3.14로 나누기)
-
-    if angle > 180.0: ## 180도 넘지 않게 조정
+    if angle > 180.0: 
         angle = 360 - angle
 
     return angle
 
-def detection(landmarks, keypoint_name): ## keypoint 좌표값 변환
-    
+# get keypoints x, y, visibility
+def detection(landmarks, keypoint_name):
     return [
         landmarks[mp_pose.PoseLandmark[keypoint_name].value].x,
         landmarks[mp_pose.PoseLandmark[keypoint_name].value].y,
         landmarks[mp_pose.PoseLandmark[keypoint_name].value].visibility
     ]
 
-def detections(landmarks): ## 좌표값 데이터값 변환
-    keypoints = pd.DataFrame(columns=["keypoint", "x", "y","visibility"]) ## keypoint 좌표값
+# get keypoints data(사용 안함)
+def detections(landmarks):
+    keypoints = pd.DataFrame(columns=["keypoint", "x", "y","visibility"])
 
-    for i, lndmrk in enumerate(mp_pose.PoseLandmark): ## 파이썬 내장함수(for문 in 뒤쪽)
+    for i, lndmrk in enumerate(mp_pose.PoseLandmark):
         lndmrk = str(lndmrk).split(".")[1]
         cord = detection(landmarks, lndmrk)
         keypoints.loc[i] = lndmrk, cord[0], cord[1], cord[2]
 
     return keypoints
 
-def table(exercise, timerer, status, set, feedback, timer,camID): ## table 표기내용
-    table = cv2.imread("./table.PNG") ## table 위치
-    cv2.putText(table, "CapstoneDisign1 Team-1", ## opencv문자열: 제목
-                (50, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, ## 문자열: 위치, 크기, 색상(검정) 설정
-                cv2.LINE_AA)
-    cv2.putText(table, "Exercise : " + exercise.replace("-", " "), ## opencv문자열: table 운동타입(입력한 운동타입)
-                (1, 75), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, ## 문자열: 위치, 크기, 색상(검정) 설정
-                cv2.LINE_AA)
-    cv2.putText(table, "Reps : " + str(timerer), (1, 110), ## opencv문자열: table 운동 카운트
+# voice feedback function
+# how to use : voiceFeedback('end') 라고 치면 end.wav 재생됨
+def voiceFeedback(sound): 
+    pygame.mixer.Sound('rest_time.wav')
+    pygame.mixer.Sound('buzzer.wav')
+    pygame.mixer.Sound('end.wav')
+     
+    if pygame.mixer.get_busy() == False:
+        return pygame.mixer.Sound(sound + '.wav').play()
+
+# make table
+def table(exercise, reps, status, sets, feedback, timer): 
+    table = cv2.imread("./table.PNG") # table 이미지 위치
+    cv2.putText(table, "Exercise            " + exercise.replace("-", " "), ## opencv문자열: table 운동타입(입력한 운동타입)
+                (1, 95), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)## 문자열: 위치, 크기, 색상(검정) 설정
+    cv2.putText(table, "Reps                " + str(reps), (1, 155), ## opencv문자열: table 운동 카운트
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA) ## 문자열: 위치, 크기, 색상(검정) 설정
-    cv2.putText(table, "Status : " + str(status), (5, 145), ## opencv문자열: table 운동 상태
+    cv2.putText(table, "Status              " + str(status), (5, 210), ## opencv문자열: table 운동 상태
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA) ## 문자열: 위치, 크기, 색상(검정) 설정
-    cv2.putText(table, "Set : " + str(set), (5, 180), ## opencv문자열: table 세트수
+    cv2.putText(table, "Set                 " + str(sets), (5, 270), ## opencv문자열: table 세트수
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA) ## 문자열: 위치, 크기, 색상(검정) 설정
-    cv2.putText(table, "Feedback : " + str(feedback), (5, 215), ## opencv문자열: table 피드백
+    cv2.putText(table, "Feedback           " + str(feedback), (5, 330), ## opencv문자열: table 피드백
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA) ## 문자열: 위치, 크기, 색상(검정) 설정
-    cv2.putText(table, "Timer : " + str(timer), (5, 250), ## opencv문자열: table 타이머
+    cv2.putText(table, "Timer               " + str(timer), (5, 390), ## opencv문자열: table 타이머
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA) ## 문자열: 위치, 크기, 색상(검정) 설정
-    cv2.putText(table, "CamID : " + str(camID), (5, 285), ## opencv문자열: table 타이머
+    cv2.imshow("Table", table) ## table 출력
+    cv2.moveWindow("Table", 0, 510)
+    
+# make angle table(사용 안함)
+def table_angle(value1, angle1, value2, angle2):
+    table_angle = cv2.imread("./table_angle.PNG")
+    cv2.putText(table_angle, "avg " + str(value1) + " : " + str(angle1), (1, 45), ## opencv문자열: table 운동 카운트
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA) ## 문자열: 위치, 크기, 색상(검정) 설정
-    cv2.imshow("Table" + str(camID), table) ## table 출력
+    cv2.putText(table_angle, "avg " + str(value2) + " : " + str(angle2), (1, 105), ## opencv문자열: table 운동 카운트
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA) ## 문자열: 위치, 크기, 색상(검정) 설정
+    cv2.imshow("Table_angle", table_angle) ## table 출력
+    cv2.moveWindow("Table_angle", 1013, 510) 
+
+# make calculations table    
+def table_calculations(**kwargs):
+    table_calculations = cv2.imread("./table_angle.PNG")
+    for i, key in enumerate(kwargs):
+        cv2.putText(table_calculations, "{} : {:.4f}".format(key, kwargs[key]), (1, 45 + i*60), ## opencv문자열: table 운동 카운트
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA) ## 문자열: 위치, 크기, 색상(검정) 설정
+    cv2.imshow("Table_calculations", table_calculations)
+    cv2.moveWindow("Table_calculations", 1013, 510) 
