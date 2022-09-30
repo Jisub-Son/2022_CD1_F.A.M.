cf81124cbf06e3b67eee87e61be38ef8ee81a39b
LEESEOKJINVOL2 <104977106+LEESEOKJINVOL2@users.noreply.github.com>
2022-09-30 19:15:10 +0900

    Add files via upload

---
+++ 캡스톤디자인2/CD_Code/main.py
@@ -0,0 +1,18 @@
+import argparse                 
+from utils import *             
+from threadingCam import camThread
+
+# argparse setting
+ap = argparse.ArgumentParser()  # python main.py -mode squat 로 실행가능
+ap.add_argument("-mode",
+                "--exercise",
+                type=str,
+                help='activity',
+                required=True)
+args = vars(ap.parse_args())
+
+# Create two threads as follows
+thread1 = camThread("Camera 0", 0, args) ## 노트북 cam
+thread2 = camThread("Camera 1", 1, args) ## usb cam
+thread1.start()
+thread2.start()
