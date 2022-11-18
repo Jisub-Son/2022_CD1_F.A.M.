9aad316fda47b23063fb7805853cbcbcc989ec2a
Odyssey <dltjrwls01@naver.com>
2022-11-18 16:38:33 +0900

    오디세이 option table 이전 반영 완료

--- Odyssey/main.py
+++ Odyssey/main.py
@@ -13,10 +13,7 @@ video_shower = VideoShow(frame1=video_getter0.frame, frame2=video_getter1.frame)
 
 print("total thread : ", active_count()) # 총 thread 확인
 
-# prevTime = 0
-
 while True:
-    curTime = time.time()
     if video_getter0.stopped or video_getter1.stopped or video_shower.stopped:
         video_shower.stop()
         video_getter1.stop()
@@ -25,8 +22,4 @@ while True:
     
     video_shower.frame1 = video_getter0.frameBuf
     video_shower.frame2 = video_getter1.frameBuf
-    
     sleep(0.034)
-    
-    # 1초 30장 = 30 fps
-    # 1 fps = 1/30 = 0.0333
